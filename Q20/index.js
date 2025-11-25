0feb4457c10a92f0095755cb029f8dc2cd43c12c
BindhuEdara <mandadi.bindu@gmail.com>
2025-11-25 13:02:27 +0530

    worked on Q20

---
+++ Q20/index.js
@@ -0,0 +1,18 @@
+function createCounter() {
+  let count = 0;
+
+  function increment() {
+    count++;
+    console.log(count);
+  }
+  function decrement() {
+    count--;
+    console.log(count);
+  }
+  return { increment, decrement };
+}
+
+const counter = createCounter();
+counter.increment();
+counter.increment();
+counter.decrement();
